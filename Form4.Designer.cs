e468bcdb881fdeff81864b549f189916d75f8a38
Vilatii <vitalii.verich@gmail.com>
2018-04-30 11:33:50 +0300

    v0.3 Rozklad

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -29,6 +29,15 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,22 +49,120 @@
             this.dataGridView1.Size = new System.Drawing.Size(329, 245);
             this.dataGridView1.TabIndex = 1;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(361, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Назва";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(361, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Код";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(523, 143);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Редагувати";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(442, 143);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Видалити";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(361, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Додати";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(442, 60);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(156, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(442, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(361, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Спеціальність";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(442, 108);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(156, 21);
+            this.comboBox1.TabIndex = 16;
+            // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(657, 261);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form4";
             this.Text = "Form4";
             this.Load += new System.EventHandler(this.Form4_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
--- Form4.cs
+++ Form4.cs
@@ -77,6 +77,62 @@ namespace Rozklad
             {
                 MessageBox.Show(ex.Message);
             }
+            DataTable SpecialnistTable = new DataTable();
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "Select * from Specialnosti";
+            MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+            adapter.Fill(SpecialnistTable);
+            comboBox1.DataSource = SpecialnistTable;
+            comboBox1.ValueMember = "Kod";
+            comboBox1.DisplayMember = "Nazva";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection(SQL.connStr);
+            conn.Open();
+            MySqlCommand comm = conn.CreateCommand();
+            comm.CommandText = "INSERT INTO Groups(Kod,Nazva,Specialnist) VALUES(@Kod, @Nazva, @Specialnist)";
+            comm.Parameters.Add("@Kod", textBox1.Text);
+            comm.Parameters.Add("@Nazva", textBox2.Text);
+            comm.Parameters.Add("@Specialnist", comboBox1.SelectedValue);
+            //MessageBox.Show("Kod= " + comboBox1.SelectedValue);
+            comm.ExecuteNonQuery();
+            mySqlDataAdapter = new MySqlDataAdapter("select * from Groups", conn);
+            DataSet DS = new DataSet();
+            mySqlDataAdapter.Fill(DS);
+            dataGridView1.DataSource = DS.Tables[0];
+            conn.Close();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection(SQL.connStr);
+            conn.Open();
+            MySqlCommand comm = conn.CreateCommand();
+            comm.CommandText = "DELETE FROM Groups WHERE Kod = " + textBox1.Text;
+            comm.ExecuteNonQuery();
+            mySqlDataAdapter = new MySqlDataAdapter("select * from Groups", conn);
+            DataSet DS = new DataSet();
+            mySqlDataAdapter.Fill(DS);
+            dataGridView1.DataSource = DS.Tables[0];
+            conn.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection(SQL.connStr);
+            conn.Open();
+            MySqlCommand comm = conn.CreateCommand();
+            comm.CommandText = "UPDATE Groups SET Nazva = '" + textBox2.Text + "', Specialnist = '" + comboBox1.SelectedValue + "' WHERE Groups.Kod = '" + textBox1.Text + "'";
+            comm.ExecuteNonQuery();
+            mySqlDataAdapter = new MySqlDataAdapter("select * from Groups", conn);
+            DataSet DS = new DataSet();
+            mySqlDataAdapter.Fill(DS);
+            dataGridView1.DataSource = DS.Tables[0];
+            conn.Close();
         }
     }
 }
